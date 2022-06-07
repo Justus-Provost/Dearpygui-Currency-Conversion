0033af86b6b75b5d5bfee9e7338061ad4c055bfa
Justus-Provost <provj709@hsd.k12.or.us>
2022-06-07 15:10:48 -0700

    Added first run

--- currency_converter/first_run.py
+++ currency_converter/first_run.py
@@ -3,11 +3,12 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=600, height=300)
 
-with dpg.window(label="Example Window"):
+with dpg.window(label="Example Window",width=584,height=261):
     dpg.add_text("Hello, world")
     dpg.add_button(label="Save")
     dpg.add_input_text(label="string", default_value="Quick brown fox")
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+    dpg.add_text("width: ",label = dpg.window)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
