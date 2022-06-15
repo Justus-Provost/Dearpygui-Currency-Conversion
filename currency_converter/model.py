a77464bdd68ac7d53e72f7c8db9223eb580f234b
Justus-Provost <provj709@hsd.k12.or.us>
2022-06-15 15:19:57 -0700

    Got working

--- currency_converter/model.py
+++ currency_converter/model.py
@@ -6,6 +6,19 @@ url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/curren
 response = requests.get(url_host)
 currency_code_map = response.json()
 
+def get_exchange_rate(from_code,to_code):
+    url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+    endpoint = from_code + "/" + to_code
+    ext = ".json"
+    url = url_host + endpoint + ext
+    response = requests.get(url)
+    if response.ok:
+        exchange = response.json()
+        rate = exchange[to_code]
+    else:
+        rate = 0
+    return rate
+
 def get_currencies():
     currency_strings = []
     for code in currency_code_map.keys():
@@ -29,3 +42,6 @@ default_to = get_currency_string("eur")
 if __name__ == "__main__":
     get_currencies()
     print(url_host)
+
+    rate = get_exchange_rate("usd","eur")
+    print(rate)
